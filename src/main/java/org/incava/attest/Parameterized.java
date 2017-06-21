59a60f24c2c1203218ed49ea894ea4f29984c7f3
Jeff Pace <jeugenepace@gmail.com>
2017-06-21 18:37:03 -0400

    Add params and paramsList method to Parameterized

--- src/main/java/org/incava/attest/Parameterized.java
+++ src/main/java/org/incava/attest/Parameterized.java
@@ -1,12 +1,20 @@
 package org.incava.attest;
 
+import java.util.List;
 import junitparams.JUnitParamsRunner;
 import org.junit.runner.RunWith;
 
 /**
  * By extending this class, subclasses do not require the RunWith(JUnitParamsRunner.class)
- * annotation.
+ * annotation and the related import statements.
  */
 @RunWith(JUnitParamsRunner.class)
 public class Parameterized {
+    public List<Object[]> paramsList(Object[] ... args) {
+        return Parameters.paramsList(args);
+    }
+
+    public static Object[] params(Object ... args) {
+        return Parameters.params(args);
+    }
 }
